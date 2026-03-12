ae5aa086090e09338e410a5624c35dc7a1e709e5
lfh <l2448808186@gmail.com>
2026-03-12 22:43:59 +0800

    chore: 版本升级至 0.3.0

--- src/voice_input/main.py
+++ src/voice_input/main.py
@@ -296,7 +296,7 @@ def main():
     parser.add_argument(
         "--version",
         action="version",
-        version="%(prog)s 0.2.0",
+        version="%(prog)s 0.3.0",
     )
     parser.add_argument(
         "--tray",
